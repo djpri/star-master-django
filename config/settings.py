b8f80f0db782c13f7335f9e3592a8998ca9c4fbb
djpri <22898729+djpri@users.noreply.github.com>
2025-09-26 21:04:49 +0100

    Update config and urls for new apps

--- config/settings.py
+++ config/settings.py
@@ -99,8 +99,8 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     # Local apps
     "config",
-    "music",
-    "chords",
+    "questions",
+    "answers",
     # django-tailwind
     "tailwind",
     "theme",
