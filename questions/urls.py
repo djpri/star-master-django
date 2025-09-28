af9c1ab1f4fc31c5bcdebb7a5b3f7a9c31cf3ab7
djpri <22898729+djpri@users.noreply.github.com>
2025-09-28 16:19:55 +0100

    Separate public and private questions to different urls

--- questions/urls.py
+++ questions/urls.py
@@ -5,5 +5,7 @@ app_name = 'questions'
 
 urlpatterns = [
     path('', views.question_list, name='list'),
+    path('public/', views.public_question_list, name='public_list'),
+    path('save/<int:question_id>/', views.save_public_question, name='save_public'),
     # path('<int:pk>/', views.question_detail, name='detail'),
 ]
