e4104e40b8d3a5401b5929b8692876c9d507bbb6
djpri <22898729+djpri@users.noreply.github.com>
2025-09-30 21:53:03 +0100

    Fix production environment collectstatic bug

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -38,9 +37,12 @@ if settings.DEBUG:
         path("__reload__/", include("django_browser_reload.urls")),
     ]
 
-if not settings.TESTING:
-    from debug_toolbar.toolbar import debug_toolbar_urls
-
-    urlpatterns = [
-        *urlpatterns,
-    ] + debug_toolbar_urls()
+if settings.DEBUG and not settings.TESTING:
+    try:
+        from debug_toolbar.toolbar import debug_toolbar_urls
+    except ModuleNotFoundError:
+        pass
+    else:
+        urlpatterns = [
+            *urlpatterns,
+        ] + debug_toolbar_urls()
